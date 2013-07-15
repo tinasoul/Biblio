dfaf8ab272d1c4ab03f43bbe9d9ca3364d8b55d5
MakerSquare Student <students@themakersquare.com>
2013-07-15 14:21:16 -0500

    Inital Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biblio::Application.config.secret_token = '2efd4ebcf290c8ae567ade89ae75161f85c7b4b2806a4752f083b946c5f7317537a8725563fe85565e1fdfdc23f5b0d2385dd8a8791dec7c880b0cd91ccd64ac'
